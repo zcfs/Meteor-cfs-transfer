508fc4f550602e100fd8eeafdf03afd91022463e
Eric Dobbertin <eric@dairystatedesigns.com>
2014-02-27 19:50:13 -0600

    move DDP upload transfer queue to new cfs-upload-ddp package

--- transfer.js
+++ transfer.js
@@ -5,11 +5,3 @@
  * There is a single downloads transfer queue per client (not per CFS)
  */
 FS.downloadQueue = new DownloadTransferQueue();
-
-/**
- * @namespace FS
- * @type UploadTransferQueue
- * 
- * There is a single uploads transfer queue per client (not per CFS)
- */
-FS.uploadQueue = new UploadTransferQueue();
